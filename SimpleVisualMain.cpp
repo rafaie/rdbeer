a428cb4d4a79c5251a777e19b8d68667fa9119d1
Mostafa <mostafa.rafaie@gmail.com>
2018-02-20 11:34:43 -0600

    Add example output result

--- SimpleVisualMain.cpp
+++ SimpleVisualMain.cpp
@@ -2,6 +2,7 @@
 #include "VisualAgent.h"
 #include <iostream>
 #include <fstream>
+#include <chrono>
 
 // Global constants
 
@@ -34,6 +35,7 @@ void run_for_line(ofstream &outFile, double StepSize, int X1, int Y1, int X2, in
   Object.SetPositionY(Y2);
   int timer = 0;
   int status = 0;
+  auto start = std::chrono::system_clock::now();
   for (double t = 0; Object.PositionY() > BodySize/2; t += StepSize) {
       // cout << "------------------\n";
       // cout << Agent.PositionX() << " " << Agent.PositionY() << " \n";
@@ -44,6 +46,12 @@ void run_for_line(ofstream &outFile, double StepSize, int X1, int Y1, int X2, in
       Agent.Step(StepSize, Object);
       Object.Step(StepSize);
   }
+  auto end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end-start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+  cout << "finished computation at " << std::ctime(&end_time)
+              << "elapsed time: " << elapsed_seconds.count() << "s\n";
+
   outFile << 1 << ',' << ++timer << ',' << StepSize << ',' << X1 << ',' << Y1 << ',' << X2 << ',' << Y2 << ',';
   outFile << Agent.PositionX() << "," << Agent.PositionY() << "," << Object.PositionX() << "," << Object.PositionY() << "," << ++status <<" \n";
 
@@ -107,16 +115,16 @@ int main(int argc, char* argv[])
     outFile.open ("output.csv");
     outFile << "obj_type,timer,step_size,X1,Y1,X2,Y2,agent_X,agent_Y,obj_X,obj_Y,status\n";
     // int X1, Y1, X2, Y2;
-    // for (int j1=150; j1 < 275; j1+=20){
-    //   for (int j2=-30; j2 <= 30; j2+=15){
-    //     for (double i = 0.1; i <= 0.2; i+= 0.05){
-    //       cout << "------------------------------------\n";
-    //       cout << i << " " << 0<< " " << j1<< " " <<-32 + int((275-j1)/2)<< " " << j2 << "\n";
-    //       run_for_line(outFile, i, j2, 0, -32 + int((275-j1)/2), j1);
-    //       run_for_Circle(outFile, i, j2, 0, -32 + int((275-j1)/2), j1);
-    //     }
-    //   }
-    // }
+    for (int j1=150; j1 < 275; j1+=20){
+      for (int j2=-30; j2 <= 30; j2+=15){
+        for (double i = 0.1; i <= 0.2; i+= 0.05){
+          cout << "------------------------------------\n";
+          cout << i << " " << 0<< " " << j1<< " " <<-32 + int((275-j1)/2)<< " " << j2 << "\n";
+          run_for_line(outFile, i, j2, 0, -32 + int((275-j1)/2), j1);
+          run_for_Circle(outFile, i, j2, 0, -32 + int((275-j1)/2), j1);
+        }
+      }
+    }
     outFile.close();
     showModel("categorize.ns");
     return 0;
